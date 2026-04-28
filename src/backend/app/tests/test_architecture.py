a8c2dba967c0851805b019475049be876136c0f0
Robert Schlee <robert.schlee@stud.th-rosenheim.de>
2026-04-28 11:08:24 +0200

    test cases erweitert

--- src/backend/app/tests/test_architecture.py
+++ src/backend/app/tests/test_architecture.py
@@ -19,6 +19,18 @@ def test_services_do_not_import_routers():
     rule.assert_applies(evaluable)
 
 
+def test_services_do_not_import_database():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_sub_modules_of("backend.app.services")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.database")
+    )
+    rule.assert_applies(evaluable)
+
+
 def test_models_do_not_import_services():
     rule = (
         Rule()
@@ -43,6 +55,18 @@ def test_models_do_not_import_routers():
     rule.assert_applies(evaluable)
 
 
+def test_models_do_not_import_database():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_sub_modules_of("backend.app.models")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.database")
+    )
+    rule.assert_applies(evaluable)
+
+
 def test_database_does_not_import_routers():
     rule = (
         Rule()
@@ -51,5 +75,29 @@ def test_database_does_not_import_routers():
         .should_not()
         .import_modules_that()
         .are_sub_modules_of("backend.app.routers")
+    )
+    rule.assert_applies(evaluable)
+
+
+def test_database_models_do_not_import_pydantic_models():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_named("backend.app.database.models")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.models")
+    )
+    rule.assert_applies(evaluable)
+
+
+def test_config_does_not_import_internal_modules():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_named("backend.app.config")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app")
     )
     rule.assert_applies(evaluable)
