401c8c3631003627c4ec5034fb027605a2bb8eb8
Robert Schlee <robert.schlee@stud.th-rosenheim.de>
2026-05-03 09:49:42 +0200

    added extra testcases

--- src/backend/app/tests/test_architecture.py
+++ src/backend/app/tests/test_architecture.py
@@ -101,3 +101,51 @@ def test_config_does_not_import_internal_modules():
         .are_sub_modules_of("backend.app")
     )
     rule.assert_applies(evaluable)
+
+
+def test_dependencies_do_not_import_routers():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_named("backend.app.dependencies")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.routers")
+    )
+    rule.assert_applies(evaluable)
+
+
+def test_dependencies_do_not_import_database():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_named("backend.app.dependencies")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.database")
+    )
+    rule.assert_applies(evaluable)
+
+
+def test_routers_do_not_import_database():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_sub_modules_of("backend.app.routers")
+        .should_not()
+        .import_modules_that()
+        .are_sub_modules_of("backend.app.database")
+    )
+    rule.assert_applies(evaluable)
+
+
+def test_services_do_not_import_dependencies():
+    rule = (
+        Rule()
+        .modules_that()
+        .are_sub_modules_of("backend.app.services")
+        .should_not()
+        .import_modules_that()
+        .are_named("backend.app.dependencies")
+    )
+    rule.assert_applies(evaluable)
